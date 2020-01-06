df27a8f2d6f23238c5f518d57415bbede977c865
Marcellofabbri <marcellogfabbri@gmail.com>
2020-01-06 15:20:29 +0000

    created app that loads pic of cat

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/cat' do
+    "<div style='border: 3px dashed red'>
+      <img src='http://bit.ly/1eze8aE' >
+    </div>"
+end
+
+get '/secret2' do
+  "message two"
+end
+
+get '/secret3' do
+    "message three"
+  end
+
