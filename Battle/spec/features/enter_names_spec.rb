14ed312186ed204f6e5d512b4b6827d9c1b72172
Marcellofabbri <marcellogfabbri@gmail.com>
2020-01-08 17:03:48 +0000

    halfway trhough implementing the Attack button

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature "enter names" do
   scenario "fill in a form, submitting names, visualizing names" do
-    visit '/'
-    fill_in :player1, with: "Uno"
-    fill_in :player2, with: "Due"
-    click_button "Submit"
-    expect(page).to have_content "Uno vs. Due"
+    sign_in_and_play
+    expect(page).to have_content "Player 1 vs. Player 2"
   end
 end
